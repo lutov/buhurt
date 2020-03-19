0447af949a88394d21385be5a1f9cf36a3f3a40a
lutov <i@vlad-lutov.name>
2020-03-19 13:22:50 +0300

    An attempt of start using webpack in this project

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,17 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js').extract().version()
+    .scripts([
+        'public/data/vendor/jquery/jquery-ui-1.12.1.custom/jquery-ui.min.js',
+        'public/data/vendor/bootstrap-star-rating/js/star-rating.min.js',
+        'public/data/vendor/rangeSlider/ion.rangeSlider-master/js/ion.rangeSlider.min.js',
+        'public/data/js/main.js'
+    ], 'public/js/main.min.js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .styles([
+       'public/data/vendor/bootstrap-star-rating/css/star-rating.min.css',
+       'public/data/vendor/rangeSlider/ion.rangeSlider-master/css/ion.rangeSlider.css',
+       'public/data/vendor/rangeSlider/ion.rangeSlider-master/css/ion.rangeSlider.skinFlat.css',
+       'public/data/css/main.css'
+   ], 'public/css/main.min.css');
