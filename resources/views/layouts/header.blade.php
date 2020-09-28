ab7314487427c57e094f1f6bb67e4ce1429ae154
lutov <i@vlad-lutov.name>
2020-09-28 15:48:52 +0300

    Update favicon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,7 +10,7 @@
     <meta name="description" content="@yield('description')"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="shortcut icon" href="https://buhurt.ru/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://buhurt.ru/favicon.svg" type="image/svg">
 
     <link href="/css/app.css" rel="stylesheet" type="text/css"/>
     <link href="/css/custom.css" rel="stylesheet" type="text/css"/>
