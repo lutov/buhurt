62f6610efdcc3a2746f10dcdd2dfa5ba9c7012aa
lutov <i@vlad-lutov.name>
2020-10-09 02:32:50 +0300

    Fix auth form

--- resources/views/sections/user/login.blade.php
+++ resources/views/sections/user/login.blade.php
@@ -25,15 +25,15 @@
                     <h1 class="card-title m-0">@yield('title')</h1>
                 </div>
                 <div class="card-body">
-                    {!! Form::open(array('action' => 'User\UserController@login', 'id' => 'entrance_form', 'class' => 'entrance_form', 'method' => 'POST')) !!}
-                    <p>{!! Form::text('email', null, array('placeholder' => 'Логин или e-mail', 'class' => 'form-control w-100')) !!}</p>
-                    <p>{!! Form::password('password', array('placeholder' => 'Пароль', 'class' => 'form-control w-100')) !!}</p>
-                    {!! Form::submit('Войти', array('class' => 'btn btn-success')) !!}
-                    <a class="btn btn-primary"
-                       href="https://oauth.vk.com/authorize?client_id=4591194&redirect_uri=https://buhurt.ru/user/vk_auth&scope=email&display=popup">
-                        <img src="https://vk.com/favicon.ico" alt="Вконтакте"/> vk.com
-                    </a>
-                    {!! Form::close() !!}
+                    <form action="/user/login/" id="entrance_form" class="entrance_form" method="POST">
+                        <p><input name="email" placeholder="Логин или e-mail" class="form-control w-100" /></p>
+                        <p><input type="password" name="password" placeholder="Пароль" class="form-control w-100" /></p>
+                        <input type="submit" value="Войти" class="btn btn-success" />
+                        <a class="btn btn-primary"
+                           href="https://oauth.vk.com/authorize?client_id=4591194&redirect_uri=https://buhurt.ru/user/vk_auth&scope=email&display=popup">
+                            <img src="https://vk.com/favicon.ico" alt="Вконтакте" style="width: 1.2rem;" /> vk.com
+                        </a>
+                    </form>
                 </div>
                 <div class="card-footer small text-muted">
                     @include('widgets.report')
