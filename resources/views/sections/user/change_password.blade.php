bb5a8b3a7b17042df5cee501f628c9b6ba3ff63f
lutov <i@vlad-lutov.name>
2020-10-10 11:22:12 +0300

    Fix change password form

--- resources/views/sections/user/change_password.blade.php
+++ resources/views/sections/user/change_password.blade.php
@@ -1,61 +1,46 @@
 @extends('layouts.default')
-
 @section('title')Смена пароля@stop
-
 @section('subtitle')@stop
-
 @section('content')
-
-    <section class="text-center mt-5 mb-3">
-        <h1 class="">@yield('title')</h1>
-    </section>
-
-    <div class="row">
-
-        <div class="col-md-12">
-
-            <div class="card w-75 p-4 m-auto">
-
-                @if(count($errors->all()))
-
-                    <section class="text-center">
-                        <h3 class="mb-3 text-danger">Ошибки</h3>
-                    </section>
-
-                    <div class="card p-4 border-danger mb-4">
-
-                        <ol class="text-danger list-unstyled">
-                            <?php
-                            //print_r($errors);
-
-                            foreach ($errors->all() as $error) {
-                                echo '<li>'.$error.'</li>';
-                            }
-                            ?>
-                        </ol>
-
+    @if(count($errors->all()))
+        <div class="row">
+            <div class="col-12 col-md-8 col-lg-6 m-auto pb-4">
+                <div class="card border-danger">
+                    <div class="card-header text-center">
+                        <h3 class="text-danger m-0">Ошибки</h3>
                     </div>
-                @endif
-
-                {!! Form::open(array('action' => 'User\UserController@change_password', 'class' => '', 'method' => 'POST')) !!}
-                <p>
-                    <label for="old_password">Текущий пароль</label>
-                    {!! Form::password('old_password', $attributes = array('id' => 'old_password', 'class' => 'form-control w-100', 'autocomplete' => 'off')) !!}
-                </p>
-
-                <p>
-                    <label for="new_password">Новый пароль</label>
-                    {!! Form::password('new_password', $attributes = array('id' => 'new_password', 'class' => 'form-control w-100')) !!}
-                </p>
-                {!! Form::submit('Сохранить', $attributes = array('id' => 'change_password')) !!}
-                {!! Form::close() !!}
-
+                    <ol class="list-group text-danger">
+                        @foreach ($errors->all() as $error)
+                            <li class="list-group-item">{!! $error !!}</li>
+                        @endforeach
+                    </ol>
+                </div>
             </div>
-
-            {!! DummyHelper::report('password') !!}
-
         </div>
-
+    @endif
+    <div class="row">
+        <div class="col-12 col-md-8 col-lg-6 m-auto pb-4">
+            <div class="card @include('card.class')" id="registration_block">
+                <div class="card-header text-center">
+                    <h1 class="card-title m-0">@yield('title')</h1>
+                </div>
+                <div class="card-body">
+                    <form action="/user/change_password/" id="entrance_form" class="entrance_form" method="POST">
+                        <p>
+                            <label for="old_password">Текущий пароль</label>
+                            <input type="password" name="old_password" id="old_password" class="form-control w-100" autocomplete="off" />
+                        </p>
+                        <p>
+                            <label for="new_password">Новый пароль</label>
+                            <input type="password" name="new_password" id="new_password" class="form-control w-100" />
+                        </p>
+                        <input type="submit" value="Сохранить" id="change_password" class="btn btn-success" />
+                    </form>
+                </div>
+                <div class="card-footer small text-muted">
+                    @include('widgets.report')
+                </div>
+            </div>
+        </div>
     </div>
-
 @stop
